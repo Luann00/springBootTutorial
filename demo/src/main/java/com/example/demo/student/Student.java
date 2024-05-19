95466d09f767e57f5d9294294073f48e97d25cf5
Luan Zekiri <luan.00@hotmail.de>
2024-05-19 17:31:08 +0200

    implemented entity student class and connected app to sql database

---
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,123 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table
+public class Student {
+	
+	
+	
+	@Id
+	private long id;
+	private String name;
+	private String eMail;
+	private LocalDate dob;
+	
+	
+	@Transient
+	private Integer age;
+	
+	public Student(long id, String name, String eMail, LocalDate dob) {
+		this.id = id;
+		this.name = name;
+		this.eMail = eMail;
+		this.dob = dob;
+	}
+	
+	
+	
+	public Student(String name, String eMail, LocalDate dob) {
+		this.name = name;
+		this.eMail = eMail;
+		this.dob = dob;
+	}
+
+
+
+	public Student() {
+		
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String geteMail() {
+		return eMail;
+	}
+
+
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+
+
+	public Integer getAge() {
+		return Period.between(dob, LocalDate.now()).getYears();
+	}
+
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", eMail=" + eMail + ", dob=" + dob + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
